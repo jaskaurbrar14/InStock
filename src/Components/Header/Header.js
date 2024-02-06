c4964b2fd348854f4f6544d72468709466bc7295
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-06 12:46:39 -0500

    Image added to Header.js file

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,2 +1,16 @@
 import React from "react";
-import logo from "../../assets/Logo/Instock-logo_1x.png";
+import InstockLogo from "../../assets/Logo/Instock-logo_1x.png";
+
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <main className="header">
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <img src={InstockLogo} className="header__image" alt="InstockImage" />
+        </div>
+      </div>
+    </main>
+  );
+};
