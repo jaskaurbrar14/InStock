9e1cd15bde0f4000ca200572afa2fd29698bc7da
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-06 13:02:41 -0500

    add footer details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <main className="main">
+        <Routes>
+        </Routes>
+      </main>
+      <Footer/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
