7e8fff74e17837132d64b0043cfeb47748306b22
Elyse <elyse.waslat@gmail.com>
2024-02-06 23:23:06 -0500

    Add warehouse component

---
+++ src/Components/WarehouseList/WarehouseList.js
@@ -0,0 +1,119 @@
+import './WarehouseList.scss';
+import deleteIcon from '../../assets/Icons/delete_outline-24px.svg';
+import editIcon from '../../assets/Icons/edit-24px.svg';
+import sortIcon from '../../assets/Icons/sort-24px.svg';
+import chevronIcon from '../../assets/Icons/chevron_right-24px.svg';
+import searchIcon from '../../assets/Icons/search-24px.svg';
+
+
+
+function WarehouseList () {
+    return (
+        <div className="warehouselist">
+            <div className="warehouselist__header"> 
+                <div className="warehouselist__header__title"> 
+                    <h1>Warehouse</h1>
+                </div>
+                <div className="warehouselist__header__search">
+
+                     <input placeholder="Search..." name="search" type="input"></input>
+                     <img className="warehouselist__header__search__icon" src={searchIcon} alt="search icon"/>
+                </div>
+                <div className="warehouselist__header__addbutton"> 
+                    <button>+ Add new Warehouse</button>
+                </div>
+            </div>   
+            
+            <div className="warehouselist__items">
+                <div className="warehouselist__items__row warehouselist__items__headerrow">
+                    <div className="warehouselist__items__row__field"> 
+                        <p>WAREHOUSE</p>
+                        <img src={sortIcon} alt="sort icon"/>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>ADDRESS</p>
+                        <img src={sortIcon} alt="sort icon"/>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>CONTACT NAME</p>
+                        <img src={sortIcon} alt="sort icon"/>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>CONTACT INFORMATION</p>  
+                        <img src={sortIcon} alt="sort icon"/>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>ACTIONS</p>
+                        <img src={sortIcon} alt="sort icon"/>
+                    </div>
+                </div>
+
+                <div className="warehouselist__items__row">
+                    <div className="warehouselist__items__row__field"> 
+                        <p><a href="#">Manhattan</a></p>
+                        <img className="warehouselist__items__row__field__name" src={chevronIcon} alt="arrow icon"/>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>503 Broadway, New York, USA</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>Parmin Aujla</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>+1 (629) 555-0129 Paulja@instock.com</p>  
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <button>
+                            <img src={deleteIcon} alt="delete button"/>
+                        </button>
+                        <button>
+                             <img src={editIcon} alt="edit button"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="warehouselist__items__row">
+                    <div className="warehouselist__items__row__field"> 
+                        <p>Manhattan</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>503 Broadway, New York, USA</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>Parmin Aujla</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>+1 (629) 555-0129 Paulja@instock.com</p>  
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <button>delete</button>
+                        <button>edit</button>
+                    </div>
+                </div>
+                <div className="warehouselist__items__row">
+                    <div className="warehouselist__items__row__field"> 
+                        <p>Manhattan</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>503 Broadway, New York, USA</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>Parmin Aujla</p>
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <p>+1 (629) 555-0129 Paulja@instock.com</p>  
+                    </div>
+                    <div className="warehouselist__items__row__field">
+                        <button>delete</button>
+                        <button>edit</button>
+                    </div>
+                </div>
+                <div className="warehouselist__items__row"></div>
+                <div className="warehouselist__items__row">Row3</div>
+                <div className="warehouselist__items__row">Row4</div>
+            </div>
+
+        </div>
+
+    )
+}
+export default WarehouseList;
