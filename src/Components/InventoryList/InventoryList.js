e0a9d80b7545bf30d9e42d43a1171e142c44101d
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-07 09:28:06 -0500

    added to list styling and organizing

--- src/Components/InventoryList/InventoryList.js
+++ src/Components/InventoryList/InventoryList.js
@@ -17,20 +17,89 @@ export default function InventoryList(){
 
 
 return(
-    <>
-    <h1 className="inventorylist__header">Inventory</h1>
-    <form className="inventorylist__form">
-      <input className="inventorylist__form--searchinput" placeholder="Search..." type="text" id="title" name="title"/>
-    </form>
-    <button type="submit" className="inventorylist__button--additem">+ Add New Item</button>
-    <ul className="inventorylist__list">
-        <li className="inventorylist__listitem">INVENTORY ITEM</li>
-        <li className="inventorylist__listitem">CATEGORY</li>
-        <li className="inventorylist__listitem">STATUS</li>
-        <li className="inventorylist__listitem">QTY</li>
-        <li className="inventorylist__listitem">WAREHOUSE</li>
-        <li className="inventorylist__listitem">ACTIONS</li>
-    </ul>
-    </>
+    <main>
+      <h1 className="inventorylist__header">Inventory</h1>
+      <form className="inventorylist__form">
+        <input className="inventorylist__form--searchinput" placeholder="Search..." type="text" id="title" name="title"/>
+      </form>
+      <button type="submit" className="inventorylist__button--additem">+ Add New Item</button>
+      <section className="inventorylist__title--parent">
+
+
+
+
+      <div className="inventorylist__detailparent">
+        <div className="inventorylist__wrapper">
+          <div className="inventorylist__detail">
+            <p className="inventorylist__title">INVENTORY ITEM</p>
+            <p className="inventorylist__value--product">Television</p>
+          </div>
+          <div className="inventorylist__detail">
+            <p className="inventorylist__title">CATEGORY</p>
+            <p className="inventorylist__value">Electronics</p>
+          </div>
+        </div>
+
+      <div className="inventorylist__wrapper">
+        <div className="inventorylist__detail">
+          <p className="inventorylist__title">STATUS</p>
+          <p className="inventorylist__value--availibility">IN STOCK</p>
+        </div>
+        <div className="inventorylist__detail">
+          <p className="inventorylist__title">QTY</p>
+          <p className="inventorylist__valuet">500</p>
+        </div>
+        <div className="inventorylist__detail">
+          <p className="inventorylist__title">WAREHOUSE</p>
+          <p className="inventorylist__value">Manhattan</p>
+        </div>
+      </div>
+      </div>
+          <p className="inventorylist__title--tabletdesktop">ACTIONS</p>
+      </section>
+
+
+
+
+
+
+      <section className="inventorylist__title--parent">
+
+
+
+
+      <div className="inventorylist__detailparent">
+        <div className="inventorylist__wrapper">
+          <div className="inventorylist__detail">
+            <p className="inventorylist__title">INVENTORY ITEM</p>
+            <p className="inventorylist__value--product">Television</p>
+          </div>
+          <div className="inventorylist__detail">
+            <p className="inventorylist__title">CATEGORY</p>
+            <p className="inventorylist__value">Electronics</p>
+          </div>
+        </div>
+
+      <div className="inventorylist__wrapper">
+        <div className="inventorylist__detail">
+          <p className="inventorylist__title">STATUS</p>
+          <p className="inventorylist__value--availibility">IN STOCK</p>
+        </div>
+        <div className="inventorylist__detail">
+          <p className="inventorylist__title">QTY</p>
+          <p className="inventorylist__valuet">500</p>
+        </div>
+        <div className="inventorylist__detail">
+          <p className="inventorylist__title">WAREHOUSE</p>
+          <p className="inventorylist__value">Manhattan</p>
+        </div>
+      </div>
+      </div>
+          <p className="inventorylist__title--tabletdesktop">ACTIONS</p>
+      </section>
+
+
+
+    </main>
     );
 }
