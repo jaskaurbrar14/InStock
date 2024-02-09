3ee09b2f773b267aa40bc04f73c9a101bc972dd5
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-09 13:59:01 -0500

    added hover color to the variables and mobile brekpoint to the mixins

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import EditWarehouse from "./Components/Edit-warehouse/editWarehouse";
 import WarehouseList from "./Components/WarehouseList/WarehouseList";
+import WarehouseInventoryList from "./Components/WarehouseInventoryList/WarehouseInventoryList";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <WarehouseList></WarehouseList>
+      {/* <WarehouseList></WarehouseList> */}
       <main className="main">
         <Routes>
           <Route element={<EditWarehouse />} />
           <Route path="/edit-warehouse" element={<EditWarehouse />} />
+          <Route path="/warehouseinventorylist" element={<WarehouseInventoryList />} />
         </Routes>
       </main>
       <Footer />
