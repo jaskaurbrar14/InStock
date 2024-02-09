10f4dc9fb854c2b7fc3fc150cdb271e96717cc0c
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-09 12:13:04 -0500

    imported InventoryList component in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import EditWarehouse from "./Components/Edit-warehouse/editWarehouse";
 import WarehouseList from "./Components/WarehouseList/WarehouseList";
+import InventoryList from "./Components/InventoryList/InventoryList";
+// import WarehouseInventoryList from "./Components/WarehouseInventoryList/WarehouseInventoryList";
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           <Route element={<EditWarehouse />} />
           <Route path="/edit-warehouse" element={<EditWarehouse />} />
           <Route path="/inventorylist" element={<InventoryList />} />
-          <Route path="/warehouseinventorylist" element={<WarehouseInventoryList />} />
+          {/* <Route path="/warehouseinventorylist" element={<WarehouseInventoryList />} /> */}
         </Routes>
       </main>
       <Footer />
