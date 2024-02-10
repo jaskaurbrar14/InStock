dc510e748fd6bf2ea5ea65519b3f7b6139fb938c
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-10 14:45:10 -0500

    resolve error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import EditWarehouse from "./Components/Edit-warehouse/editWarehouse";
 import WarehouseList from "./Components/WarehouseList/WarehouseList";
+import AddInventory from "./Components/Add-Inventory/add-inventory";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<WarehouseList />} />
           <Route path="/edit-warehouse" element={<EditWarehouse />} />
+          <Route path="/add-inventory" element={<AddInventory />} />
         </Routes>
       </main>
       <Footer />
