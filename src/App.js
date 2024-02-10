331f667de7da119c75a993485f1f7ab9f9e5ab23
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-10 13:58:02 -0500

    file modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import EditWarehouse from "./Components/Edit-warehouse/editWarehouse";
 import AddWarehouse from "./Components/AddWarehouse/AddWarehouse";
+import WarehouseDetails from "./Components/WarehouseDetails/WarehouseDetails";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/edit-warehouse" element={<EditWarehouse />} />
           <Route element={<AddWarehouse />} />
           <Route path="/AddWarehouse" element={<AddWarehouse />} />
+          <Route element={<WarehouseDetails />} />
+          <Route path="/WarehouseDetails" element={<WarehouseDetails />} />
         </Routes>
       </main>
       <Footer />
--- src/Components/WarehouseDetails/WarehouseDetails.js
+++ src/Components/WarehouseDetails/WarehouseDetails.js
@@ -1,72 +1,121 @@
-import React from "react";
-import edit from "../../assets/Icons/edit-24px.svg";
-import backArrow from "../../assets/Icons/arrow_back-24px.svg";
-import "./InventoryItemDetails.scss";
-import { Link } from "react-router-dom";
-function WarehouseDetails() {
-  return (
-    <section className="Inventory">
-      <div className="Inventory__header">
-        <Link className="Inventory__header-backlink">
-          <img
-            className="Inventory__header-backlink-img"
-            src={backArrow}
-            alt="back arrow icon"
-          />
-        </Link>{" "}
-        <h1 className="Inventory__header-heading">TELEVISION</h1>
-        <Link className="Inventory__header-editlink">
-          <img
-            className="Inventory__header-editlink-img"
-            src={edit}
-            alt="edit icon"
-          />
-          <p className="Inventory__header-editlink-text">Edit</p>
-        </Link>
-      </div>
-      <hr className="Inventory__divider" />
-      <article className="Inventory__details">
-        <div className="Inventory__details-container">
-          <div className="Inventory__details-container-description">
-            <p className="Inventory__details-container-description-label">
-              Item description
-            </p>
-            <p className="Inventory__details-container-description-text">
-              This 50", 4K LED TV provides a crystal-clear picture and vivid
-              colors.
-            </p>
-          </div>
-          <div className="Inventory__details-container-category">
-            <p className="Inventory__details-container-category-label">
-              Category
-            </p>
-            <p className="Inventory__details-container-category-text">
-              Electronics
-            </p>
-          </div>
-        </div>
-        <hr className="Inventory__details-divider" />
-        <div className="Inventory__details-stock">
-          <div className="Inventory__details-stock-status">
-            {" "}
-            <p className="Inventory__details-stock-status-label">Status</p>
-            <p className="Inventory__details-stock-status-text">In Stock</p>
-          </div>
-          <div className="Inventory__details-stock-quantity">
-            {" "}
-            <p className="Inventory__details-stock-quantity-label">Quantity</p>
-            <p className="Inventory__details-stock-quantity-text">500</p>
-          </div>
-          <div className="Inventory__details-stock-warehouse">
-            {" "}
-            <p className="Inventory__details-stock-warehouse-label">
-              Warehouse
-            </p>
-            <p className="Inventory__details-stock-warehouse-text">Manhattan</p>
-          </div>
-        </div>
-      </article>
-    </section>
-  );
-}
-export default WarehouseDetails;
+// import React, { useState } from "react";
+// import Sort from "../../assets/icons/sort-24px.svg";
+// import Delete from "../../assets/icons/delete_outline-24px.svg";
+// import Edit from "../../assets/icons/edit-24px.svg";
+// import Right from "../../assets/icons/chevron_right-24px.svg";
+// import Arrow from "../../assets/icons/arrow_back-24px.svg";
+// import { Link } from "react-router-dom";
+// import DeleteItem from "../DeleteItem/DeleteItem";
+// import axios from "axios";
+// import "../InventoryList/InventoryList.scss";
+
+// const WarehouseDetails = ({ warehousesList, inventoryList }) => {
+//   const [display, setDisplay] = useState(false);
+//   const [singleItem, setSingleItem] = useState({});
+
+//   const showModal = () => {
+//     setDisplay(true);
+//   };
+
+//   const cancelModal = () => {
+//     setDisplay(false);
+//   };
+
+//   const deleteInventory = (
+//     id,
+//     warehouseID,
+//     warehouseName,
+//     itemName,
+//     description,
+//     category,
+//     status,
+//     quantity
+//   ) => {
+//     const deletedInventory = {
+//       id: id,
+//       warehouseID: warehouseID,
+//       warehouseName: warehouseName,
+//       itemName: itemName,
+//       description: description,
+//       category: category,
+//       status: status,
+//       quantity: quantity,
+//     };
+
+//     axios
+//       .delete("http://localhost:8080/inventories", deletedInventory)
+//       .then((res) => {
+//         console.log(res.data);
+//       })
+//       .catch((err) => console.log(err));
+//   };
+
+//   const findItem = (id, warehouseID) => {
+//     const foundIt = inventoryList.find(
+//       (item) => item.id === id && item.warehouseID === warehouseID
+//     );
+//     console.log(foundIt);
+
+//     setSingleItem(foundIt);
+//   };
+
+//   return (
+//     <>
+//       {warehousesList.map((item) => (
+//         <div className="warehouseInventoryLocation" key={item.id}>
+//           <div className="warehouseInventoryLocation__name-container">
+//             <div className="warehouseInventoryLocation__img-name-container">
+//               <img src={Arrow} alt="arrow back" />
+//               <h1 className="warehouseInventoryLocation__name">{item.name}</h1>
+//             </div>
+//             <button className="warehouseInventoryLocation__button">
+//               {" "}
+//               Edit
+//             </button>
+//           </div>
+//           <hr className="warehouseInventoryLocation__hr" />
+//           <div className="warehouseInventoryLocation__sub">
+//             <div className="warehouseInventoryLocation__address-container">
+//               <h3 className="warehouseInventoryLocation__address-heading">
+//                 WAREHOUSE ADDRESS:
+//               </h3>
+//               <p className="warehouseInventoryLocation__address">
+//                 {item.address}
+//               </p>
+//               <p className="warehouseInventoryLocation__city">
+//                 {item.city}, {item.country}
+//               </p>
+//             </div>
+//             <div className="warehouseInventoryLocation__left-container">
+//               <div className="warehouseInventoryLocation__contact-name-container">
+//                 <h3 className="warehouseInventoryLocation__contact-name-heading">
+//                   CONTACT NAME:
+//                 </h3>
+//                 <p className="warehouseInventoryLocation__contact-name">
+//                   {item.contact.name}
+//                 </p>
+//                 <p className="warehouseInventoryLocation__contact-position">
+//                   {item.contact.position}
+//                 </p>
+//               </div>
+//               <div className="warehouseInventoryLocation__contact-information-container">
+//                 <h3 className="warehouseInventoryLocation__contact-information-heading">
+//                   CONTACT INFORMATION
+//                 </h3>
+//                 <p className="warehouseInventoryLocation__contact-information-phone">
+//                   {item.contact.phone}
+//                 </p>
+//                 <p className="warehouseInventoryLocation__contact-information-email">
+//                   {item.contact.email}
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//       {/* Rest of your JSX code */}
+//     </>
+//   );
+// };
+
+// export default WarehouseDetails;
